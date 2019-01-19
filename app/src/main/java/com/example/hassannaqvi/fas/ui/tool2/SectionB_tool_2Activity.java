f9ffe7f3b3dabc4ea01f6617f0b3019ad506fc2d
syed.irfanullah <irfansyed.pg@gmail.com>
2019-01-19 17:43:28 +0500

    Save Drafts Sectionb_tool_2

--- app/src/main/java/com/example/hassannaqvi/fas/ui/tool2/SectionB_tool_2Activity.java
+++ app/src/main/java/com/example/hassannaqvi/fas/ui/tool2/SectionB_tool_2Activity.java
@@ -1,18 +1,26 @@
 package com.example.hassannaqvi.fas.ui.tool2;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.example.hassannaqvi.fas.R;
+import com.example.hassannaqvi.fas.databinding.ActivitySectionBTool2Binding;
+
+import org.json.JSONObject;
 
 public class SectionB_tool_2Activity extends AppCompatActivity {
+
+
+    ActivitySectionBTool2Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section_b_tool_2);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_b_tool_2);
+        bi.setCallback(this);
     }
 
 
@@ -33,6 +41,16 @@ public class SectionB_tool_2Activity extends AppCompatActivity {
     }
 
     private void SaveDraft() {
+
+        JSONObject s02 = new JSONObject();
+
+
+        s02.put("fas01a00",
+                bi.fas02a00a.isChecked() ? "1"
+                        : bi.fas02a00b.isChecked() ? "2"
+                        : bi.fas02a00c.isChecked() ? "3"
+                        : bi.fas02a00d.isChecked() ? "4" : "0");
+
     }
 
     private boolean formValidation() {
