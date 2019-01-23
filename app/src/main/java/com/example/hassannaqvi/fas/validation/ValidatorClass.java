cfefb80ec2bad07061db4c64a9bcc3a9b5719c6b
ali.azaz <ali.azaz.alam@hotmail.com>
2019-01-23 18:21:45 +0500

    some changes in validation file

--- app/src/main/java/com/example/hassannaqvi/fas/validation/ValidatorClass.java
+++ app/src/main/java/com/example/hassannaqvi/fas/validation/ValidatorClass.java
@@ -150,9 +150,22 @@ public abstract class ValidatorClass {
             Log.i(context.getClass().getName(), context.getResources().getResourceEntryName(rdGrp.getId()) + ": This data is Required!");
             return false;
         } else {
-            rdBtn.setError(null);
-            rdBtn.clearFocus();
-            return true;
+            boolean rdbFlag = true;
+            for (int j = 0; j < rdGrp.getChildCount(); j++) {
+                View innerV = rdGrp.getChildAt(j);
+                if (innerV instanceof EditText) {
+                    if (getIDComponent(rdGrp.findViewById(rdGrp.getCheckedRadioButtonId())).equals(innerV.getTag()))
+                        rdbFlag = EmptyTextBox(context, (EditText) innerV, getString(context, getIDComponent(innerV)));
+                }
+            }
+
+            if (rdbFlag) {
+                rdBtn.setError(null);
+                rdBtn.clearFocus();
+                return rdbFlag;
+            } else
+                return rdbFlag;
+
         }
     }
 
