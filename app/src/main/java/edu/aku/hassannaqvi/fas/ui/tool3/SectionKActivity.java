e43ff2d47dc7b6a9de2f8267e2416f33a9b7ce84
SAJID LATIF <indus1530@gmail.com>
2020-05-09 12:42:21 +0500

    tool3 - jk

--- app/src/main/java/edu/aku/hassannaqvi/fas/ui/tool3/SectionKActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/fas/ui/tool3/SectionKActivity.java
@@ -1,4 +1,4 @@
-package edu.aku.hassannaqvi.fas.ui.tool1;
+package edu.aku.hassannaqvi.fas.ui.tool3;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
@@ -10,34 +10,31 @@ import org.json.JSONObject;
 
 import java.util.concurrent.ExecutionException;
 
-import edu.aku.hassannaqvi.fas.JSON.GeneratorClass;
 import edu.aku.hassannaqvi.fas.R;
 import edu.aku.hassannaqvi.fas.RMOperations.crudOperations;
 import edu.aku.hassannaqvi.fas.core.CONSTANTS;
 import edu.aku.hassannaqvi.fas.core.MainApp;
 import edu.aku.hassannaqvi.fas.data.DAO.FormsDAO;
 import edu.aku.hassannaqvi.fas.data.entities.Forms;
-import edu.aku.hassannaqvi.fas.databinding.ActivitySectionCBinding;
-import edu.aku.hassannaqvi.fas.databinding.ActivitySectionJBinding;
+import edu.aku.hassannaqvi.fas.databinding.ActivitySectionKBinding;
 import edu.aku.hassannaqvi.fas.ui.EndingActivity;
-import edu.aku.hassannaqvi.fas.validation.ClearClass;
+import edu.aku.hassannaqvi.fas.ui.tool1.SectionDActivity;
 import edu.aku.hassannaqvi.fas.validation.ValidatorClass;
 
-public class SectionJActivity extends AppCompatActivity {
+public class SectionKActivity extends AppCompatActivity {
 
-    ActivitySectionJBinding bi;
+    ActivitySectionKBinding bi;
     private Forms fc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_j);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_k);
         bi.setCallback(this);
 
         setContentUI();
         setListeners();
     }
-
     private void setListeners() {
 
 //        bi.hfa1332.addTextChangedListener(new TextWatcher() {
@@ -81,7 +78,7 @@ public class SectionJActivity extends AppCompatActivity {
         fc = (Forms) getIntent().getSerializableExtra(CONSTANTS._URI_FC_OBJ);
 
 
-        /*ClearClass.ClearAllFields(bi.hfa1300, false);
+       /* ClearClass.ClearAllFields(bi.hfa1300, false);
         String getSurvey = MainApp.getParamValue(this, CONSTANTS._URI_DATAMAP_SURVEY_TYPE);
         if (!getSurvey.equals("0"))
             bi.hfa1300.check(bi.hfa1300.getChildAt(Integer.valueOf(getSurvey) - 1).getId());
@@ -125,35 +122,59 @@ public class SectionJActivity extends AppCompatActivity {
     }
 
     private void SaveDraft() throws JSONException {
-
         JSONObject json = new JSONObject();
-
-        json.put("ax11", bi.ax11.getText().toString());
-        json.put("ax12", bi.ax12.getText().toString());
-        json.put("ax13", bi.ax13.getText().toString());
-
-        json.put("ax14", bi.ax14a.isChecked() ? "1"
-                : bi.ax14b.isChecked() ? "2"
-                : bi.ax14c.isChecked() ? "3"
-                : bi.ax14d.isChecked() ? "4"
+        json.put("ax21a", bi.ax21a.getText().toString());
+        json.put("ax21b", bi.ax21b.getText().toString());
+        json.put("ax21c", bi.ax21c.getText().toString());
+        json.put("ax21d", bi.ax21d.getText().toString());
+        json.put("ax21e", bi.ax21e.getText().toString());
+        json.put("ax21f", bi.ax21f.getText().toString());
+        json.put("ax21g", bi.ax21g.getText().toString());
+        json.put("ax22", bi.ax22.getText().toString());
+        json.put("ax23", bi.ax23a.isChecked() ? "1"
+                : bi.ax23b.isChecked() ? "2"
+                : bi.ax23c.isChecked() ? "3"
+                : bi.ax23d.isChecked() ? "98"
                 : "-1");
-
-        json.put("ax15", bi.ax15.getText().toString());
-
-        json.put("ax16", bi.ax16a.isChecked() ? "1"
-                : bi.ax16b.isChecked() ? "2"
-                : bi.ax16c.isChecked() ? "3"
+        json.put("ax23cx", bi.ax23cx.getText().toString());
+        json.put("ax24", bi.ax24a.isChecked() ? "1"
+                : bi.ax24b.isChecked() ? "2"
+                : bi.ax24c.isChecked() ? "3"
+                : bi.ax24d.isChecked() ? "4"
+                : bi.ax24e.isChecked() ? "5"
+                : bi.ax24f.isChecked() ? "98"
                 : "-1");
+        json.put("ax25",
+                bi.ax25a.isChecked() ? "1" :
+                        bi.ax25b.isChecked() ? "2" :
+                                bi.ax25c.isChecked() ? "3" :
+                                        bi.ax25d.isChecked() ? "4" :
+                                                bi.ax25e.isChecked() ? "5" :
+                                                        bi.ax25f.isChecked() ? "98" :
+                                                                "-1");
+        json.put("ax26",
+                bi.ax26a.isChecked() ? "1" :
+                        bi.ax26b.isChecked() ? "2" :
+                                bi.ax26c.isChecked() ? "66" :
+                                        bi.ax26d.isChecked() ? "98" :
+                                                "-1");
+        json.put("ax27",
+                bi.ax27a.isChecked() ? "1" :
+                        bi.ax27b.isChecked() ? "2" :
+                                bi.ax27c.isChecked() ? "3" :
+                                        bi.ax27d.isChecked() ? "98" :
+                                                "-1");
+        json.put("ax271",
+                bi.ax271a.isChecked() ? "1" :
+                        bi.ax271b.isChecked() ? "2" :
+                                bi.ax271c.isChecked() ? "96" :
+                                        "-1");
+        json.put("ax271cx", bi.ax271cx.getText().toString());
 
-        json.put("ax16bx", bi.ax16bx.getText().toString());
-
-        json.put("ax16cx", bi.ax16cx.getText().toString());
-
-        fc.setSa3(String.valueOf(json));
     }
 
     private boolean formValidation() {
-        return ValidatorClass.EmptyCheckingContainer(this, bi.GrpName);
+        return ValidatorClass.EmptyCheckingContainer(this, bi.GrpNameSectionK);
     }
 
     public void BtnEnd() {
